246c6ae51056c8a226f65c72320fd321939f01ea
Halleh Khoshnevis <hallehkhoshnevis@ip-10-15-1-240.eu-central-1.compute.internal>
2018-01-04 12:21:31 +0100

    symmetric signature generation based on payconiq algorithm

---
+++ SignatureApi/SignatureGenerationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SignatureApi
+{
+    /**
+     * Class to specify the exceptions which can occure during signature generation or verification
+     **/
+    public class SignatureGenerationException: Exception 
+    {
+        public SignatureGenerationException(String message): base(message)
+        {}
+    }
+}
